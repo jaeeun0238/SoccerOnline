09349b8985f924970eb2800bcc235e13a4bba775
김지웅 <sizera1004@gmail.com>
2024-12-02 17:49:00 +0900

    fix: 에러처리 통일을 위한 수정

--- routers/user_Router.js
+++ routers/user_Router.js
@@ -16,8 +16,9 @@ router.patch('/buyCash', async (req, res, next) => {
     const [tokenType, token] = authorization.split(' '); // bearer과 token값 객체구조분해 할당
 
     // 토큰형식 확인, middleware로 따로 빼도 됨
-    if (tokenType !== 'Bearer')
-      return res.status(401).json({ error: '토큰 형식이 잘못되었습니다.' });
+    if (tokenType !== 'Bearer') {
+      return next(errModel(401, '토큰 형식이 잘못되었습니다.'));
+    }
 
     const decodedToken = jwt.verify(token, process.env.JWT_KEY); // 토큰 원상태로, 키 부분 이름은 추후 수정
     const userPID = decodedToken.userPID; //토큰의 userPID부분 가져옴
@@ -29,19 +30,17 @@ router.patch('/buyCash', async (req, res, next) => {
 
     // userPID가 존재하지 않으면 에러 반환, middleware로 따로 빼도 됨
     if (!user) {
-      return res
-        .status(404)
-        .json({ error: '일치하는 사용자가 존재하지 않습니다.' });
+      return next(errModel(404, '일치하는 사용자가 존재하지 않습니다.'));
     }
 
     // cash가 존재하지 않으면 에러, middleware로 따로 빼도 됨
     if (!cash) {
-      return res.status(400).json({ error: '캐시를 입력해 주세요' });
+      return next(errModel(400, '캐시를 입력해 주세요.'));
     }
 
     // cash가 유효한 값인지 검사, middleware로 따로 빼도 됨
     if (typeof cash !== 'number' || cash < 0) {
-      return res.status(400).json({ error: '유효한 캐시 값을 입력해 주세요' });
+      return next(errModel(400, '유효한 캐시 값을 입력해 주세요.'));
     }
 
     // 캐시 추가해서 업데이트
@@ -52,6 +51,6 @@ router.patch('/buyCash', async (req, res, next) => {
 
     return res.status(200).json({ currentCash: updateCash.userCash });
   } catch (err) {
-    errorMiddelware(err); // app.js에 추가한다면 next(err)로 수정가능
+    next(err);
   }
 });
