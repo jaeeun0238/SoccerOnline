3540cee22617b8b4aa92133583a39a5be3bbdcf9
lgm-7 <xlzlrud@naver.com>
2024-12-03 15:17:50 +0900

    update game_Router
    오류발생시 미들웨어처리

--- routers/game_Router.js
+++ routers/game_Router.js
@@ -6,49 +6,56 @@ const router = express.Router();
 
 //게임 생성  //parameter에 userPID 입력
 router.post('/game-start/:userPID', async (req, res, next) => {
-  const mySquad = req.params;
-  const enemySquad = req.body;
+  try {
+    const mySquad = req.params;
+    const enemySquad = req.body;
 
-  //parameter에 입력한 유저PID를 가지고 있는 스쿼드정보
-  const mySquadInfo = await prisma.player_squads_Data.findFirst({
-    where: {
-      userPID: +mySquad,
-    },
-  });
+    //parameter에 입력한 유저PID를 가지고 있는 스쿼드정보
+    const mySquadInfo = await prisma.player_squads_Data.findFirst({
+      where: {
+        userPID: +mySquad,
+      },
+    });
 
-  //body에서 입력한 스쿼드PID를 가진 스쿼드정보
-  const enemySquadInfo = await prisma.player_squads_Data.findFirst({
-    where: {
-      player_squadsPID: +enemySquad,
-    },
-  });
+    //body에서 입력한 스쿼드PID를 가진 스쿼드정보
+    const enemySquadInfo = await prisma.player_squads_Data.findFirst({
+      where: {
+        player_squadsPID: +enemySquad,
+      },
+    });
 
-  //수정필요
-  const mySquadScore = mySquadInfo.strikerPosition.playerAbility_ATCK;
-  const enemySquadScore = enemySquadInfo.strikerPosition.playerAbility_ATCK;
+    //수정필요
+    const mySquadScore = mySquadInfo.strikerPosition.playerAbility_ATCK;
+    const enemySquadScore = enemySquadInfo.strikerPosition.playerAbility_ATCK;
 
-  const maxScore = mySquadScore + enemySquadScore;
+    const maxScore = mySquadScore + enemySquadScore;
 
-  const randomValue = Math.random() * maxScore;
-  if (randomValue < mySquadScore) {
-    // 유저 승리 처리
-    const myScore = Math.floor(Math.random() * 4) + 2; // 2에서 5 사이
-    const enemyScore = Math.floor(Math.random() * Math.min(3, myScore)); // aScore보다 작은 값을 설정
-    result = `승리: 유저 ${myScore} - ${enemyScore} 상대`;
-  } else {
-    // 상대 유저 승리 처리
-    const myScore = Math.floor(Math.random() * 4) + 2; // 2에서 5 사이
-    const enemyScore = Math.floor(Math.random() * Math.min(3, enemyScore)); // bScore보다 작은 값을 설정
-    result = `패배: 상대 ${enemyScore} - ${myScore} 유저`;
+    const randomValue = Math.random() * maxScore;
+
+    let myScore, enemyScore, result;
+
+    if (randomValue < mySquadScore) {
+      // 유저 승리 처리
+      myScore = Math.floor(Math.random() * 4) + 2; // 2에서 5 사이
+      enemyScore = Math.floor(Math.random() * Math.min(3, myScore)); // aScore보다 작은 값을 설정
+      result = `승리: 유저 ${myScore} - ${enemyScore} 상대`;
+    } else {
+      // 상대 유저 승리 처리
+      myScore = Math.floor(Math.random() * 4) + 2; // 2에서 5 사이
+      enemyScore = Math.floor(Math.random() * Math.min(3, enemyScore)); // bScore보다 작은 값을 설정
+      result = `패배: 상대 ${enemyScore} - ${myScore} 유저`;
+    }
+
+    //게임 전적
+    await prisma.game_Records.create({
+      data: {
+        gameRecord: result,
+      },
+    });
+    return res.status(200).json({ data: result });
+  } catch (err) {
+    next(err);
   }
-
-  //게임 전적
-  await prisma.game_Records.create({
-    data: {
-      gameRecord: result,
-    },
-  });
-  return res.status(200).json({ data: result });
 });
 
 export default router;
