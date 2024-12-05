ed8ac4782f9f379bf1da45f6bec70ac643896e21
WooK1184 <qkrtjddnr03@gmail.com>
2024-12-05 16:26:34 +0900

    feat player_Router.js
    선수 강화 api 추가

--- routers/player_Router.js
+++ routers/player_Router.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-import { authenticateJWT } from '../middlewares/auth.middleware.js';
+import authenticateJWT from '../middlewares/auth.middleware.js';
 import { prisma } from '../uts/prisma/index.js';
 
 const router = express.Router();
+
 // 플레이어생성 api
 router.post('/player/make', async (req, res, next) => {
   try {
@@ -117,5 +118,102 @@ router.post('/player/gacha', authenticateJWT, async (req, res) => {
     player: result.player,
   });
 });
+
+
+//선수 강화 API
+const upgradePlayer = async (req, res, next) => {
+  
+  const maxUpgrade = 10 //최대 강화 단계
+
+  try {
+    const { userPID } = req.user //인증된 유저 정보 가져오기
+    const { playerID, materials } = req.body
+
+    const user = await prisma.userData.findUnique({
+      where: { userPID }, //유저 정보 조회
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    //보유 선수 확인
+    const existingPlayer = await prisma.playerRostersData.findFirst({
+      where: {
+        userPID,
+        playerPID: playerID
+      },
+    });
+
+    if (!existingPlayer) {
+      throw new Error('Player is not exist')
+    }
+
+    //강화 재료 확인
+    if (!materials || !Array.isArray(materials) || materials.length < 1) {
+      return res
+        .status(400)
+        .json({ message: '강화에 최소 1명의 선수가 필요합니다.' });
+    }
+
+    const materialCheck = await prisma.playerRostersData.findMany({
+      where: {
+        userPID,
+        playerPID: { in: materials },
+      },
+    });
+
+    //강화 재료가 유저 보유 선수인지 확인
+    if (materialCheck.length !== materials.length) {
+      return res
+        .status(400)
+        .json({ message: '강화 재료로 사용할 선수는 보유 선수가 아닙니다.' });
+    }
+
+    // const avgAbility = await prisma.playerRostersData.findMany({
+    //   where: {
+    //     playerID
+    //   },
+    // }); 선수 오버롤 작업
+
+    const currentUpgradeLevel = existingPlayer.playerEnchant || 0; // 현재 강화 단계
+    if (currentUpgradeLevel >= maxUpgrade) {
+      return res
+        .status(400)
+        .json({ message: '선수는 이미 최대 강화 단계에 도달했습니다.' });
+    }
+
+    // const upgradePercent = 재료 선수 5명의 오버롤 기준 강화 확률 계산
+
+    //선수 강화
+    const updateEnchant = await prisma.playerData.update({
+      where: { playerPID: playerID },
+      data: {
+        playerAbilityATCK: existingPlayer.playerAbilityATCK + 5,
+        playerAbilityDEFEND: existingPlayer.playerAbilityDEFEND + 5,
+        playerAbilityMOBILITY: existingPlayer.playerAbilityMOBILITY + 5,
+        playerEnchant: existingPlayer.playerEnchant + 1,
+      },
+    });
+
+    // 강화 재료 삭제
+    await prisma.playerRostersData.deleteMany({
+      where: {
+        userPID,
+        playerPID: { in: materials },
+      },
+    });
+
+    return res.status(200).json({
+      message: `선수 강화가 완료되었습니다!)`,
+      updateEnchant,
+    });
+  } catch (err) {
+    next(err);
+    }
+}
+
+//선수 강화 API
+router.put('/player/upgrade', authenticateJWT, upgradePlayer);
 
 export default router;
